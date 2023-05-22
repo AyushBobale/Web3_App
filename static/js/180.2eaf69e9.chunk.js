09ba9981515c8bb5e30e4e7ad62f72146c985104
Ayush Bobale <bobaleayush@gmail.com>
2023-05-22 09:11:43 +0530

    Updates

---
+++ static/js/180.2eaf69e9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[180],{9180:function(e,o,t){t.r(o);var n=null;"undefined"!==typeof WebSocket?n=WebSocket:"undefined"!==typeof MozWebSocket?n=MozWebSocket:"undefined"!==typeof t.g?n=t.g.WebSocket||t.g.MozWebSocket:"undefined"!==typeof window?n=window.WebSocket||window.MozWebSocket:"undefined"!==typeof self&&(n=self.WebSocket||self.MozWebSocket),o.default=n}}]);
+//# sourceMappingURL=180.2eaf69e9.chunk.js.map
